4acf3b8727a9f0e105be27c63539a10feb1725f2
Michael Krisher <mike@mikekrisher.com>
2024-11-06 15:17:07 -0500

    Update user.lua
    activates common plugins

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -82,4 +82,61 @@ return {
       )
     end,
   },
+
+
+  {
+    "EdenEast/nightfox.nvim",
+    -- as = "nightfox",
+    config = function()
+      require("nightfox").setup()
+    end,
+  },
+  {
+    "dstein64/vim-startuptime",
+    -- lazy-load on a command
+    cmd = "StartupTime",
+    -- init is called during startup. Configuration for vim plugins typically should be set in an init function
+    init = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
+  {
+    "folke/twilight.nvim",
+    lazy = false,
+    enabled = true,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+  {
+    "nvim-zh/colorful-winsep.nvim",
+    config = true,
+    event = { "WinNew" },
+  },
+  { "tpope/vim-rails",          ft = "ruby" },
+  { "tpope/vim-abolish",        lazy = false },
+  { "tpope/vim-surround",       lazy = false },
+  { "tpope/vim-unimpaired",     lazy = false },
+  { "vim-scripts/Align",        lazy = false },
+  { "folke/todo-comments.nvim", lazy = false },
+  { "keith/rspec.vim",          ft = "ruby" },
+  { 'neovim/nvim-lspconfig' },
+  { 'simrat39/rust-tools.nvim' },
 }
